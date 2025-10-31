4c211e28f1fcf9d2f0ad71b30ceb46f2b74acd2e
brunoMyguelDotCom <brunomyguelmarcall@gmail.com>
2025-10-31 02:30:31 -0300

    Initial commit: Implementação básica do sistema de recuperação de senha por e-mail em Java

---
+++ src/main/java/com/mycompany/teste_email/RecuperacaoSenhaEmail.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste_email;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RecuperacaoSenhaEmail {
+    
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        System.out.print("Digite o e-mail do cliente: ");
+        String emailCliente = scanner.nextLine();
+
+        // gera codigo aleatorio
+        String codigo = String.format("%06d", random.nextInt(1_000_000));
+
+        // config gmail
+        String smtpHost = "smtp.gmail.com";
+        int smtpPort = 587;
+        String smtpUser = "todoit623@gmail.com";
+        String smtpPass = "irlaewxuaijjahki"; 
+
+        // config smtp
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", smtpPort);
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(smtpUser, smtpPass);
+            }
+        });
+
+        // monta email e envia
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(smtpUser));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailCliente));
+        message.setSubject("TODOIT - Código de Recuperação de Senha");
+        message.setText("Olá!\nSeu código de recuperação é: " + codigo +
+                "\nDigite este código no software para redefinir sua senha.");
+
+        Transport.send(message);
+        System.out.println("Código enviado para o e-mail do cliente!");
+
+        System.out.print("Digite o código recebido: ");
+        String codigoRecebido = scanner.nextLine();
+
+        // Verifica se o código está correto
+        if (codigo.equals(codigoRecebido)) {
+            System.out.println("Código correto! Você pode redefinir a senha.");
+        } else {
+            System.out.println("Código incorreto. Tente novamente.");
+        }
+
+        scanner.close();
+    }
+}
